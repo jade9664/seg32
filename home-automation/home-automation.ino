e97633b4d052dc761bc713983f05137fa961320f
David Ibia <david.ibia@boxmarshall.com>
2017-03-06 18:19:35 +0100

    Added POST Function

--- home-automation/home-automation.ino
+++ home-automation/home-automation.ino
@@ -1,4 +1,20 @@
+#include <ArduinoJson.h>
+#include <SoftwareSerial.h>
 
+#define DEBUG true
+
+SoftwareSerial esp8266(2,3); // make RX Arduino line is pin 2, make TX Arduino line is pin 3.
+                             // This means that you need to connect the TX line from the esp to the Arduino's pin 2
+                             // and the RX line from the esp to the Arduino's pin 3
+
+String ssid = "B68A-57F8"
+String key = "B2F66D4B"
+
+String server="www.heftytips.com"
+String url = "microcontroller.php"
+
+byte dat[5];
+                             
 int MaxVoltage; //overvoltage value
 int MaxCurrent; //overcurrent value
 int MinVoltage; //undervoltage value
@@ -14,7 +30,9 @@ char __loadProfile[] = "{'fan':1,'socket':2,'light':3}";
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  esp.begin(115200);
+  init();
 }
 
 void loop() {
@@ -22,6 +40,32 @@ void loop() {
 
 }
 
+void init(){
+  // initialization procedures for the esp module
+  reset();
+  connectWifi()[
+}
+
+void reset(){
+  esp.println("AT+RST");
+  delay(1000);
+  if (esp.find("OK")){
+    Serial.println("Module Reset")
+  }
+}
+
+void connectWifi(){
+  String syn = "AT+CWJAP=\" + ssid +"\",""+key+"\"";
+  esp.println(syn);
+  delay(4000);
+  if(esp.find("OK")){
+    Serial.println("Connected to " + ssid +".");
+  }
+  else{
+    connectWifi();
+    Serial.println("Cannot connect to "+ ssid + ".");
+  }
+}
 
 float getVoltage(){
   //placeholder
@@ -54,7 +98,8 @@ float getCurrent(){
 
 bool cutoffPower(){
   //**critical function**
-  //trigger relay to disconnect source
+  digitalWrite(mainPowerPin,LOW);
+  return true
 }
 
 
@@ -115,9 +160,36 @@ void pushLocalStat(){
 }
 char __get(){
   //helper function for get
+  if(esp8266.available()){
+    
+  }
 }
 
-char __post(){
+char __post(data){
   //helper function for post
+  esp.println("AT-CIPSTART=\"TCP\",\""+server +"\",80"); //Start a TCP Connection
+  if (esp.find("OK)){
+   Serial.println("TCP Connection Ready");
+  }
+  delay(1000);
+
+  String postRequest = "POST " + url + " HTTP/1.0\r\n" + "Host: " + server + "\r\n" + "Accept: *" + "/" + "*\r\n" + " Content-Length: " + data.length() + "'\r\n" + "Content-Type: application/x-www-form-urlencoded\r\n" + "\r\n" + data;
+  String sendsyn = "AT+CIPSEND="; //determine the number of characters to be sent
+
+  esp.print(sendsyn);
+  esp.println(postRequest.length());
+  delay(500);
+  if(esp.find(">")){
+  Serial.println("Sending...");
+  esp.print(postRequest);
+  if(esp.find("SEND OK")){
+    Serial.println("Packet sent");
+    while (esp.available()){
+      String responce = esp.readString();
+      Serial.println(responce);
+    }
+    esp.println("AT+CIPCLOSE");
+  }
 }
+
 
